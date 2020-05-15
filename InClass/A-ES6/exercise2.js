904ccee499962c27ffc6839aaa0224d255b9a9c0
akondshaheen <darknightcandle@gmail.com>
2020-05-15 18:17:36 +0200

    amended code-completed

--- InClass/A-ES6/exercise2.js
+++ InClass/A-ES6/exercise2.js
@@ -29,22 +29,46 @@ class Rectangle extends Polygon {
 
 let r = new Rectangle(50, 60);
 r.sayName();
-//r.area = 3;
+
 console.log('The area of this polygon is ' + r.area);
 
 //Create 2 new classes that extend from polygon: triangle and circle and log their names and areas
 
-var triangle = new Polygon(25, 15);
-console.log(triangle.name);
-const areasTriangle = (triangle.height * triangle.width) / 2;
-console.log(areasTriangle);
+
+//extended class Triangle
+class Triangle2 extends Polygon {
+  constructor(height, width) {
+    super(height, width);
+    this.name = "Triangle";
+  }
+  get areasTriangle() { return (this.height * this.width) / 2; }
+  sayName() {
+    console.log('Hi, I am a Polygon and my name is ' + this.name + ".");
+  }
+}
+var triangle = new Triangle2(12, 25);
+triangle.sayName();
+console.log("Total areas of this rectangle is " + triangle.areasTriangle + ".")
 
 
-const circle = new Polygon(10, 25);
-console.log(circle.name);
-const radius = ((circle.height) / 2) + ((circle.width * circle.width) / (8 * circle.height));
-const areasCircle = Math.PI * (radius * radius);
-console.log(areasCircle);
 
+//extended class circle
+class Circle extends Polygon {
+  constructor(height, width) {
+    super(height, width);
+    this.name = "circle";
+  }
+  get areasCircle() {
+    const radius = ((circle.height) / 2) + ((circle.width * circle.width) / (8 * circle.height));
+    const areasCircle = Math.PI * (radius * radius);
+    return areasCircle;
+  }
+  sayName() {
+    console.log('Hi, I am a Polygon and my name is ' + this.name + ".");
+  }
+}
+var circle = new Circle(12, 25);
+circle.sayName();
+console.log("Total areas of this rectangle is " + circle.areasCircle + ".");
 
 
